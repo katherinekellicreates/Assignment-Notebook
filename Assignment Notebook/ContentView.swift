fd7258de6c66f08568fb661d4e7e13a2ad890549
Kat <katherinekelli.creates@gmail.com>
2026-01-28 10:40:36 -0600

    Created Assignment Item struct and array

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems =
+    [AssignmentItem(course: "Algebra", description: "Linear Equation", dueDate: Date()),
+     AssignmentItem(course: "History", description: "Civil War Paper", dueDate: Date()),
+     AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date())]
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,4 +25,11 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
